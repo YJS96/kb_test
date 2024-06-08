65a5aeea04b3305c10924a4b23fa798db4ead162
YJS96 <wnt9864@hotmail.com>
2024-06-08 23:38:32 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="upper"></div>
+    <RouterView />
+    <div class="bottom"></div>
+  </div>
+</template>
+
+<style scoped></style>
+
+<style>
+/* Pretendard */
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css');
+
+* {
+  font-family: 'Pretendard Variable', Pretendard, -apple-system,
+    BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+    'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+
+  --gray900: #191f28;
+  --white: #fdfdfd;
+  color: var(--gray900);
+}
+
+#app {
+  height: 100dvh;
+  min-width: 300px;
+  max-width: 600px;
+  padding-top: env(safe-area-inset-top);
+  background-color: black;
+  margin: 0 auto;
+  position: relative;
+}
+
+.upper {
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  top: 10px;
+  background-color: var(--white);
+}
+
+.bottom {
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  bottom: 10px;
+  background-color: var(--white);
+}
+</style>
